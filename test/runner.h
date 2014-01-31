3bb1c4f2590407e29649f1fa1c80e968904ef8df
Dylan Cali <calid1984@gmail.com>
2014-01-31 10:57:30 -0600

    test: support flexibly setting custom task options
    Add a single TEST_ENTRY_CUSTOM hook that can be used to override task
    entry defaults. Different tests can have different timeouts depending on
    what is appropriate for each test. A separate TEST_OUTPUT_ENTRY hook is
    no longer necessary.
    
    In order to support per-task timeouts, the timeout field has been moved
    into the task_entry_t struct. The default (5000) is now set as part of
    TEST_ENTRY.

--- test/runner.h
+++ test/runner.h
@@ -41,6 +41,9 @@ typedef struct {
   int (*main)(void);
   int is_helper;
   int show_output;
+
+  /* The time in milliseconds after which a single test times out. */
+  int timeout;
 } task_entry_t, bench_entry_t;
 
 
@@ -58,10 +61,10 @@ typedef struct {
   int run_test_##name(void);
 
 #define TEST_ENTRY(name)                            \
-    { #name, #name, &run_test_##name, 0, 0 },
+    { #name, #name, &run_test_##name, 0, 0, 5000 },
 
-#define TEST_OUTPUT_ENTRY(name)                     \
-    { #name, #name, &run_test_##name, 0, 1 },
+#define TEST_ENTRY_CUSTOM(name, is_helper, show_output, timeout) \
+    { #name, #name, &run_test_##name, is_helper, show_output, timeout },
 
 #define BENCHMARK_DECLARE(name)                     \
   int run_benchmark_##name(void);
@@ -97,13 +100,12 @@ extern task_entry_t TASKS[];
 /*
  * Run all tests.
  */
-int run_tests(int timeout, int benchmark_output);
+int run_tests(int benchmark_output);
 
 /*
  * Run a single test. Starts up any helpers.
  */
 int run_test(const char* test,
-             int timeout,
              int benchmark_output,
              int test_count);
 
